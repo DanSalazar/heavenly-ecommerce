bcdefa20218a9d3e05333b99676207fbf15051ec
DanSalazar <danielsalazar.cc@gmail.com>
2024-05-06 02:14:12 -0400

    Add anim to shopping bag component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,18 +24,18 @@ const links: NavLinks[] = [
 
 export default function Navbar() {
   return (
-    <header className="h-16 border-b border-zinc-200 grid grid-cols-2 md:grid-cols-3 items-center">
-      <nav className="hidden md:flex gap-4">
+    <header className="relative z-20 h-16 border-b border-zinc-200 grid grid-cols-2 md:grid-cols-3">
+      <nav className="hidden md:flex self-center gap-4">
         {links.map(link => (
           <Link className="uppercase font-medium" href={link.path}>
             {link.link}
           </Link>
         ))}
       </nav>
-      <div className="flex items-center md:justify-center">
+      <div className="bg-white flex items-center md:justify-center">
         <HeavenlyIcon />
       </div>
-      <div className="z-20 self-stretch flex gap-2 justify-end items-center">
+      <div className="self-stretch flex gap-2 justify-end items-center bg-white">
         <HeartIcon />
         <div className='relative h-full flex items-center group'>
           <ShoppingBagIcon />
