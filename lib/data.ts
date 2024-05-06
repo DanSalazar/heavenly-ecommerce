3603e72ef9696bde782c21ddbc2d49d35a377272
DanSalazar <danielsalazar.cc@gmail.com>
2024-05-06 15:12:37 -0400

    Add mock data

---
+++ lib/data.ts
@@ -0,0 +1,20 @@
+interface Product {
+  img_src: string;
+  title: string;
+  price: number;
+  discount: boolean;
+  discount_number: number;
+  department: string;
+  category: string;
+}
+
+export const product: Product = {
+  img_src:
+    'https://utfs.io/f/581dce5b-a7cb-4407-999a-989883cee2d0-9l3n4k.webp',
+  title: "New ANINE BING 100% cashmere slim knit sweater women's sweater",
+  price: 100,
+  discount: false,
+  discount_number: 0,
+  department: 'men',
+  category: 'shoes'
+}
