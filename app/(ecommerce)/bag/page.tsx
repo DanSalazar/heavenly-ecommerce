b58e0beb1549b87f9799e1a561e0f1bcc8e85128
DanSalazar <danielsalazar.cc@gmail.com>
2024-05-06 15:43:42 -0400

    Add product row in bag

--- app/(ecommerce)/bag/page.tsx
+++ app/(ecommerce)/bag/page.tsx
@@ -1,5 +1,9 @@
 import Price from "@/components/ecommerce/price"
+import { Truck } from "@/components/icons"
+import ProductRow from "@/components/shopping-bag/product-row"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
+import { product } from "@/lib/data"
 
 type SumProps = {
   description: string
@@ -27,6 +31,12 @@ export default function Page() {
           <h2 className="font-semibold uppercase text-6xl">Bag</h2>
           <span className="font-medium mt-2">1</span>
         </header>
+        <div>
+          <ProductRow product={product} />
+           <ProductRow product={product} />
+            <ProductRow product={product} />
+             <ProductRow product={product} />
+        </div>
       </div>
       <div className="flex flex-col gap-4">
         <h2 className="text-4xl font-semibold break-words">Summary</h2>
@@ -36,6 +46,20 @@ export default function Page() {
         </div>
         <Sum description="Total" price={SHIPPING_PRICE + total} />
         <Button className="h-10">Checkout</Button>
+
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <span className="flex gap-2">
+                <Truck/> Free Shipping
+              </span>
+            </AccordionTrigger>
+            <AccordionContent>
+              Your order qualifies for free shipping. Join US for free
+              shipping on every order, every time
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
 	)
---
+++ components/shopping-bag/quantity-selector.tsx
@@ -0,0 +1,23 @@
+import { MinusIcon, PlusIcon } from '../icons'
+import { Button } from '../ui/button'
+
+export function QuantitySelector() {
+  return (
+    <div className="flex gap-2 items-center">
+      <p className="hidden md:block text-sm font-medium">Quantity:</p>
+      <Button
+        size={'sm'}
+        variant={'ghost'}>
+        <MinusIcon/>
+      </Button>
+      <div className="border border-zinc-300 text-sm xs:text-base p-2 px-4 flex justify-center items-center">
+        1
+      </div>
+      <Button
+        size={'sm'}
+        variant={'ghost'}>
+        <PlusIcon/>
+      </Button>
+    </div>
+  )
+}
