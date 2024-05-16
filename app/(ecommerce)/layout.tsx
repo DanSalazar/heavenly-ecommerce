bb47a9e0079a37400b1d1406e343fe01480adb16
DanSalazar <danielsalazar.cc@gmail.com>
2024-05-15 20:51:27 -0400

    Add basic filters managment

--- app/(ecommerce)/layout.tsx
+++ app/(ecommerce)/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + ' flex flex-col gap-4 px-6'}>
+      <body style={{
+        padding: '0 1.5rem'
+      }} className={inter.className + ' flex flex-col gap-4'}>
         <Navbar />
         {children}
         <Footer />
--- components/filter/filter-products.tsx
+++ components/filter/filter-products.tsx
@@ -6,11 +6,50 @@ import { MarkIcon } from '../icons'
 import SortBy from './sort-by'
 import { Filter, FilterChildren } from '.'
 import { Input } from '../ui/input'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '../ui/select'
+
+const FilterSelect = ({
+  title,
+  children,
+  handleChange
+}: {
+  title: string
+  children: React.ReactNode
+  handleChange: (title: string, value: string) => void
+}) => (
+  <FilterChildren title={title}>
+    <Select onValueChange={value => handleChange(title, value)}>
+      <SelectTrigger>
+        <SelectValue placeholder={title} />
+      </SelectTrigger>
+      <SelectContent>{children}</SelectContent>
+    </Select>
+  </FilterChildren>
+)
 
 export default function FilterProducts() {
   const [open, setOpen] = useState(false)
+  const params = useSearchParams()
+  const { replace } = useRouter()
+  const pathname = usePathname()
 
   const handleOpen = () => setOpen(!open)
+
+  const handleChange = (title: string, value: string) => {
+    const newParams = new URLSearchParams(params)
+
+    if (value) newParams.set(title.toLowerCase(), value)
+    else newParams.delete(title.toLowerCase())
+
+    replace(pathname + '?' + newParams.toString())
+  }
 
   return (
     <div className="h-10 relative flex gap-2">
@@ -19,21 +58,27 @@ export default function FilterProducts() {
       </Button>
       <div className="my-1 border-r border-zinc-300" />
       <div className="hidden md:flex gap-2 w-[80%] overflow-hidden">
-        <Button variant={'custom'}>
-          Sneakers <MarkIcon className="ml-1" />
-        </Button>
-        <Button variant={'custom'}>
-          Size M <MarkIcon className="ml-1" />
-        </Button>
+        {[...params.values()].map(filter => (
+          <Button className="capitalize" variant={'custom'}>
+            {filter} <MarkIcon className="ml-1" />
+          </Button>
+        ))}
       </div>
       <SortBy />
       <Filter
         onClose={handleOpen}
         className="absolute top-12 left-0 w-full md:w-[300px]"
         open={open}>
-        <FilterChildren title="Sizes">
-          <Input placeholder="Size" />
-        </FilterChildren>
+        <FilterSelect title="Sizes" handleChange={handleChange}>
+          {['XS', 'S', 'M', 'L'].map(item => (
+            <SelectItem value={item}>{item}</SelectItem>
+          ))}
+        </FilterSelect>
+        <FilterSelect title="Category" handleChange={handleChange}>
+          {['Shoes', 'Sneakers', 'Shirts', 'Jeans'].map(item => (
+            <SelectItem value={item}>{item}</SelectItem>
+          ))}
+        </FilterSelect>
       </Filter>
     </div>
   )
