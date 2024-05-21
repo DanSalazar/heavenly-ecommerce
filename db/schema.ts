fa255e22d8dbdd783d2cc3d6d712f00e72eb7616
DanSalazar <danielsalazar.cc@gmail.com>
2024-05-20 21:38:30 -0400

    Add database schemas

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,10 @@
+import { sql } from 'drizzle-orm'
 import {
   boolean,
   date,
   integer,
   pgTableCreator,
+  serial,
   text,
   varchar
 } from 'drizzle-orm/pg-core'
@@ -10,28 +12,54 @@ import {
 const createTable = pgTableCreator(name => 'heavenly_' + name)
 
 export const product = createTable('product', {
-  id: integer('id').primaryKey(),
-  name: varchar('name', { length: 50 }).notNull(),
+  id: varchar('id', { length: 255 }).primaryKey().notNull(),
+  name: varchar('name', { length: 255 }).notNull(),
   brand: varchar('brand', { length: 50 }),
   description: text('description'),
   price: integer('price').notNull(),
   discount: boolean('discount').default(false),
   percentage_off: integer('percentage_off'),
-  image: varchar('image', { length: 255 }).notNull(),
-  color: varchar('color', { length: 20 }).notNull(),
-  sizes: varchar('sizes', { length: 255 }).notNull(),
   department: varchar('department', {
     length: 20,
     enum: ['men', 'women']
   }).notNull(),
-  category: varchar('category', { length: 20 }).notNull()
+})
+
+export const color = createTable('color', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 30 })
+})
+
+export const size = createTable('size', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 15 })
+})
+
+export const category = createTable('category', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 20 })
+})
+
+export const product_type = createTable('product_type', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 20 })
+})
+
+export const productVariations = createTable('product_variations', {
+  id: serial('id').primaryKey(),
+  product_id: varchar('product_id').references(() => product.id),
+  color_id: serial('color_id').references(() => color.id),
+  size_id: serial('size_id').references(() => size.id),
+  product_type_Id: serial('product_type_id').references(() => product_type.id),
+  category_id: serial('category_id').references(() => category.id),
+  stock: integer('stock')
 })
 
 export const bagItem = createTable('bag_item', {
-  id: varchar('id', { length: 100 }),
-  productId: integer('productId')
-    .unique()
-    .references(() => product.id),
+  id: serial('id').primaryKey(),
+  item_id: serial('item_id')
+    .references(() => productVariations.id),
   quantity: integer('quantity'),
-  createdAt: date('createdAt').defaultNow()
+  createdAt: date('createdAt').defaultNow(),
+  expiresAt: date('expiresAt').default(sql`current_timestamp + interval '24 hours'`),
 })
