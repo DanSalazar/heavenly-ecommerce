8ccc3c7dfa500fa19ef9ef2946bb367e710579f8
DanSalazar <danielsalazar.cc@gmail.com>
2025-05-12 04:00:51 -0400

    fix: update import path for OurFileRouter

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,10 +1,9 @@
+import { OurFileRouter } from '@/app/[locale]/api/uploadthing/core'
 import {
   generateReactHelpers,
   generateUploadButton,
   generateUploadDropzone
 } from '@uploadthing/react'
-
-import type { OurFileRouter } from '@/app/api/uploadthing/core'
 
 export const UploadButton = generateUploadButton<OurFileRouter>()
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
